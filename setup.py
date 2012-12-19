848a73477d5d5ff6677b292dd0f1feb1ba7ca61d
Jon Swanson <jswan2@uw.edu>
2012-12-18 16:30:44 -0800

    fixed syntax error in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'labgeeks-hermes'
+    name = 'labgeeks-hermes',
     version = '1.0',
     license = 'Apache',
     url = 'http://github.com/abztrakt/labgeeks_hermes',
